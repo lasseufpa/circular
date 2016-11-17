5685a4c1be454b1c16bc8451035657af8c7f8f06
Paulo Albertklik <paulo.albertklik@gmail.com>
2016-11-17 04:02:04 -0300

    atualizações no aplicativo de demonstração

---
+++ AndroidApp/app/src/main/java/org/lasseufpa/circular/circular.java
@@ -0,0 +1,16 @@
+package org.lasseufpa.circular;
+
+/**
+ * Created by alberto on 17/11/2016.
+ */
+
+public class circular {
+    double currentX;
+    double currentY;
+    int velocidade;
+    int nome;
+
+
+
+
+}
