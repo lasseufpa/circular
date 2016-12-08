4e4ee3d80ae1f30643fa6f38b4568f5c0dafb23c
AlanTas <alantas@outlook.com>
2016-12-08 10:28:57 -0300

    Serviços de localização adicionados

--- CheckLocation/app/src/main/java/org/example/circular/checklocation/MainActivity.java
+++ CheckLocation/app/src/main/java/org/example/circular/checklocation/MainActivity.java
@@ -1,13 +1,114 @@
 package org.example.circular.checklocation;
 
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+import android.Manifest;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.FusedLocationProviderApi;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationListener;
+
+public class MainActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, com.google.android.gms.location.LocationListener {
+
+    private TextView latitude;   // Variável de classe para armazenar view de latitude
+    private TextView longitude;  // Variável de classe para armazenar view de longitude
+    private Button boton;        //Variável de classe para armazenar view de latitude
+    private GoogleApiClient googleApiClient; // Variável de classe para armazenar instãncia de cliente do Google API
+    private Location Localiza;
+    private LocationRequest locationRequest;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Criando uma instancia da GoogleAPIClient
+        if (googleApiClient == null) {
+            googleApiClient = new GoogleApiClient.Builder(this)
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this)
+                    .addApi(LocationServices.API)
+                    .build();
+        }
+
+        createLocationRequest();
+
+        latitude = (TextView) findViewById(R.id.latitudeView);
+        longitude = (TextView) findViewById(R.id.longitudeView);
+    }
+
+
+    protected void onStart() {
+        googleApiClient.connect();
+        super.onStart();
+    }
+
+    protected void onStop() {
+        googleApiClient.disconnect();
+        super.onStop();
+    }
+
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+
+        startLocationUpdates();
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+
+    protected void createLocationRequest() {
+        locationRequest = new LocationRequest();
+        locationRequest.setInterval(10 * 1000);
+        locationRequest.setFastestInterval(5 * 1000);
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+    }
+
+    protected void startLocationUpdates() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+            //Exibe um toast caso a permissão para acesso aos serviços de localização não esteja dada
+            Toast.makeText(this, "Abilite o acesso do app aos serviços de localização", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        LocationServices.FusedLocationApi.requestLocationUpdates(googleApiClient, locationRequest, this);
+
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+
+        Localiza = location;
+        updateUI();
+    }
+
+    protected void updateUI(){
+        latitude.setText(String.valueOf(Localiza.getLatitude()));
+        longitude.setText(String.valueOf(Localiza.getLongitude()));
+        Toast.makeText(this, "Localização atualizada", Toast.LENGTH_LONG).show();
+
     }
 }
