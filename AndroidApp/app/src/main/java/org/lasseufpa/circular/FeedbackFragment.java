c40d3523bf40930d6039788f43e81123fdf2e965
AlanTas <alantas@outlook.com>
2017-04-04 21:44:43 -0300

    Itens do menu do drawer

---
+++ AndroidApp/app/src/main/java/org/lasseufpa/circular/FeedbackFragment.java
@@ -0,0 +1,18 @@
+package org.lasseufpa.circular;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class FeedbackFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_feedback, container, false);
+    }
+}
