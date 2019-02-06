8e8a5f188ac80f20d7e131497dcaa2eafd246615
Camila Novaes <novaes.csw@gmail.com>
2019-02-06 09:59:47 -0300

    fix createtable script

--- Embarcado/mqtt-sql/sql/scripts/CreateTable.sql
+++ Embarcado/mqtt-sql/sql/scripts/CreateTable.sql
@@ -1,7 +1,7 @@
 CREATE TABLE dados (
-    id number(5),
-    timer varchar(20),
-    lat varchar(20),
-    lng varchar(20),
-    speed varchar(10)
+    id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    timer VARCHAR(20),
+    lat VARCHAR(20),
+    lng VARCHAR(20),
+    speed VARCHAR(10)
 );
