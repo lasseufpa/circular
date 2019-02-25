e7c29c442da31ccca726d3b7efaee4ea97f6acab
Izidio carvalho <izidiocarvalho88@gmail.com>
2019-02-25 11:05:55 -0300

    route added

--- Site/3.0/application/views/mapa.php
+++ Site/3.0/application/views/mapa.php
@@ -19,6 +19,8 @@
     <script type="text/javascript" src="assets/js/mqtt/browserMqtt.js"></script>
     <script type="text/javascript" src="assets/js/mqtt/clientMqtt.js"></script>
 
+	<script type="text/javascript" src="assets/js/layer/vector/KML.js"></script>
+		
 </head>
 <body>
 <div id="mapid" style="width: 100vw; height: 100vh;"></div>
