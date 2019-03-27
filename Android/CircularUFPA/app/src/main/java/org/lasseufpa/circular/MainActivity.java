50eb85fcae2ed3ed3e1968b12eddb1991a18fd59
Camila Novaes <novaes.csw@gmail.com>
2019-03-27 13:58:02 -0300

    editing sidebar menu

--- Android/CircularUFPA/app/src/main/java/org/lasseufpa/circular/MainActivity.java
+++ Android/CircularUFPA/app/src/main/java/org/lasseufpa/circular/MainActivity.java
@@ -80,17 +80,17 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_map) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_active_fleet) {
 
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_stops) {
 
         } else if (id == R.id.nav_share) {
 
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_feedback) {
+
+        } else if (id == R.id.nav_about) {
 
         }
 
