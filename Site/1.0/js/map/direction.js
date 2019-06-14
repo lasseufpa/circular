992c6921d010e3451470f621d44bcf2a4270a0bc
Camila Novaes <novaes.csw@gmail.com>
2019-06-13 22:34:35 -0300

    Reorganizing 'Site' folder
    - Delete old website versions
    - Rename folders

--- Site/1.0/js/map/direction.js
+++
@@ -1,4 +0,0 @@
-function redirect(){
-	location.href="http://http://circularufpa.esy.es/index.php";
-}
-
--- Site/1.0/js/menu/menu.js
+++
@@ -1,6 +0,0 @@
-function paginaSobre(){ 
-location.href="http://circularufpa.esy.es/sobre.html";
-} 
-function paginaInicial(){ 
-location.href="http://circularufpa.esy.es/index.html";
-}
